d81336eae634aa66fa3edcbad020bef2c2f7e695
Alex Widener <awidener3@gmail.com>
2022-03-02 09:25:13 -0500

    finished 2nd part of tutorial: game now has basic functionality

--- README.md
+++ README.md
@@ -37,3 +37,93 @@ Card is a **React component class/type** which takes in the parameter `title`, w
 `render()` returns a **React element** which describes what will be rendered.
 
 The text within the `return()` is called **JSX**, which is essentially a representation of HTML inside of JavaScript.
+
+### State
+
+State is how React elements "remember" things in a program (i.e. has the element been clicked?).
+
+State can be defined in the components constructor and are *private* to a React component that it's defined in.
+
+> There are other, simpler ways to do this outside of a JavaScript class.
+
+```js
+class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        // Initialize state
+        this.state = {
+            value: null,
+        }
+    }
+
+    // methods, render, etc...
+}
+```
+> When using a constructor/JavaScript classes, always start with a `super(props)` call
+
+To adjust state, you can use an `onClick()` within the `render()` to `setState`
+
+```js
+class Square extends React.Component {
+    
+    // ...
+
+    return(
+        <button 
+            className="square" 
+            onClick={() => { 
+                this.setState({value: 'X'})
+            }}
+        >
+            {this.state.value}
+        </button>
+    )
+}
+```
+
+`this.setState` essentially re-renders that individual square whenever it is clicked.
+
+### Lifting State
+
+Lifting state is when you take a state of a component and pass it **upwards** to it's parent. In this example, the `Square` components parent is the `Board`. This is how we can track the game itself.
+
+### Immutability
+
+Immutability is when an original object is left in it's original state and a "copy" is created when it is modified. This allows for features like:
+* Game history
+* Undo/Redo
+
+It allows changes to be detected easily, since you have the original copy to reference.
+
+In React, it helps build *pure components*
+
+### Function Components
+
+Function components are a simpler way to write components that only contain a render method and don't have their own state.
+
+```js
+// Class component
+class Square extends React.Component {
+    render() {
+        return (
+            <button
+				className="square"
+				onClick={() => {
+					this.props.onClick();
+				}}
+			>
+				{this.props.value}
+			</button>
+		);
+    }
+}
+
+// Function component
+function Square(props) {
+    return(
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
+}
+```
--- src/index.js
+++ src/index.js
@@ -3,24 +3,136 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 // React Component #1: Square
-class Square extends React.Component {
-	// Renders a single button
-	render() {
-		return <button className="square">{/* TODO */}</button>;
-	}
+// class Square extends React.Component {
+// * (4) Constructor to add state to the component
+// * (9) Remove constructor, square no longer keeps track of game state
+// constructor(props) {
+// 	// Always call super() in a constructor
+// 	super(props);
+// 	// Initialize state
+// 	this.state = {
+// 		value: null,
+// 	};
+// }
+
+// * (1) Renders a single button
+// render() {
+// * (2) Pass in value of i as text
+// return <button className="square">{this.props.value}</button>;
+
+// return (
+// * (3) onClick adds listener to each square, must be a function!
+// <button
+// 	className="square"
+// 	onClick={() => {
+// 		console.log('click');
+// 	}}
+// >
+// 	{this.props.value}
+// </button>
+
+// * (5) Replace `this.props.value` with `this.state.value`
+// * Change onClick func to set that state to 'X'
+// <button
+// 	className="square"
+// 	onClick={() => {
+// 		this.setState({ value: 'X' });
+// 	}}
+// >
+// 	{this.state.value}
+// </button>
+
+// * (11) Replace this.state.value with this.props.value
+// * Replace setState() with props.onClick()
+// 			<button
+// 				className="square"
+// 				onClick={() => {
+// 					this.props.onClick();
+// 				}}
+// 			>
+// 				{this.props.value}
+// 			</button>
+// 		);
+// 	}
+// }
+
+// React Component #1: Square (rewritten)
+// * (13) Square component rewritten using function component
+// Be sure to remove `this`!
+function Square(props) {
+	return (
+		<button className="square" onClick={props.onClick}>
+			{props.value}
+		</button>
+	);
 }
 
 // React Component #2: Board
 class Board extends React.Component {
+	// * (6) Add constructor to set initial state
+	constructor(props) {
+		super(props);
+		this.state = {
+			squares: Array(9).fill(null),
+			// * (14) Add xIsNext to help track turn
+			xIsNext: true,
+		};
+	}
+
+	// * (12) Define handleClick
+	// handleClick(i) {
+	// 	// .slice() makes a copy of the squares array to modify
+	// 	const squares = this.state.squares.slice();
+	// 	squares[i] = 'X';
+	// 	this.setState({ squares: squares });
+	// }
+
+	// * (15) Update handleClick to flip boolean on xIsNext
+	handleClick(i) {
+		const squares = this.state.squares.slice();
+		// * (19) Add in logic to check for a winner
+		if (calculateWinner(squares) || squares[i]) {
+			return;
+		}
+		// Add logic to change X or O
+		squares[i] = this.state.xIsNext ? 'X' : 'O';
+		this.setState({
+			squares: squares,
+			// Flips boolean
+			xIsNext: !this.state.xIsNext,
+		});
+	}
+
 	// Calls the Square React component
 	renderSquare(i) {
 		// pass a prop called 'value'
-		return <Square value={i} />;
+		// return <Square value={i} />;
+
+		// * (7) Add prop passing to instruct square of its current value (X, O, null)
+		// return <Square value={this.state.squares[i]} />;
+
+		// * (8) Pass down function to handle square state, since component state is private
+		return (
+			<Square
+				value={this.state.squares[i]}
+				onClick={() => this.handleClick(i)}
+			/>
+		);
 	}
 
 	// Renders 9 squares
 	render() {
-		const status = 'Next player: X';
+		// * (16) Update status text to reflect turn
+		// const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+
+		// * (18) Update status text to also show winner
+		const winner = calculateWinner(this.state.squares);
+		let status;
+		if (winner) {
+			status = 'Winner: ' + winner;
+		} else {
+			status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+		}
 
 		return (
 			<div>
@@ -66,3 +178,28 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(<Game />, document.getElementById('root'));
+
+// * (17) logic to declare winner
+function calculateWinner(squares) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+		if (
+			squares[a] &&
+			squares[a] === squares[b] &&
+			squares[a] === squares[c]
+		) {
+			return squares[a];
+		}
+	}
+	return null;
+}
